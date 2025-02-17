0d911f92493c6400679ac0bbed392640bf33c13c
notkulonme <nricsi006@gmail.com>
2025-02-17 18:13:21 +0100

    adding gender dist, memebership dist functionality and better logging

--- src/main/kotlin/Serialization.kt
+++ src/main/kotlin/Serialization.kt
@@ -11,19 +11,23 @@ import io.ktor.server.plugins.contentnegotiation.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import kotlinx.serialization.json.Json
+import org.slf4j.LoggerFactory
 
 fun Application.configureSerialization() {
     install(ContentNegotiation) {
         json()
     }
-    val json = Json { prettyPrint=true }
+
+    val json = Json { prettyPrint = true }
     val client = HttpClient(OkHttp)
     val url = environment.config.property("api.database_url").getString()
+
+
     routing {
 
         get("/customers/count") {
             val response = processGet(client, url)
-            if (response == null){
+            if (response == null) {
                 call.respond(HttpStatusCode.InternalServerError, "Database error")
                 return@get
             }
@@ -31,9 +35,9 @@ fun Application.configureSerialization() {
             call.respond(mapOf("count" to customerList.size))
         }
 
-        get("/customers/avg-age"){
+        get("/customers/avg-age") {
             val response = processGet(client, url)
-            if (response == null){
+            if (response == null) {
                 call.respond(HttpStatusCode.InternalServerError, "Database error")
                 return@get
             }
@@ -42,9 +46,9 @@ fun Application.configureSerialization() {
             val avgAge = customerList.mapNotNull { it.age }.average()
             call.respond(mapOf("avgAge" to avgAge))
         }
-        get("/customers/most-frequent-purchase-category"){
+        get("/customers/most-frequent-purchase-category") {
             val response = processGet(client, url)
-            if (response == null){
+            if (response == null) {
                 call.respond(HttpStatusCode.InternalServerError, "Database error")
                 return@get
             }
@@ -58,20 +62,20 @@ fun Application.configureSerialization() {
             call.respond(mapOf("preferedCategory" to category))
         }
 
-        get("/customers/sum-of-purchase"){
+        get("/customers/sum-of-purchase") {
             val response = processGet(client, url)
-            if (response == null){
+            if (response == null) {
                 call.respond(HttpStatusCode.InternalServerError, "Database error")
                 return@get
             }
             val customerList = json.decodeFromString<ArrayList<Customer>>(response)
-            val sumOfPurchase = customerList.sumOf { it.totalSpending }
+            val sumOfPurchase = customerList.sumOf { it.totalSpending }.toLong()
             call.respond(mapOf("sumOfPurchase" to sumOfPurchase))
         }
 
-        get("/customers/avg-order-value"){
+        get("/customers/avg-order-value") {
             val response = processGet(client, url)
-            if (response == null){
+            if (response == null) {
                 call.respond(HttpStatusCode.InternalServerError, "Database error")
                 return@get
             }
@@ -80,9 +84,9 @@ fun Application.configureSerialization() {
             call.respond(mapOf("avgOrderValue" to averageValue))
         }
 
-        get("/customers/purchase-frequency"){
+        get("/customers/purchase-frequency") {
             val response = processGet(client, url)
-            if (response == null){
+            if (response == null) {
                 call.respond(HttpStatusCode.InternalServerError, "Database error")
                 return@get
             }
@@ -90,12 +94,50 @@ fun Application.configureSerialization() {
             val averageValue = customerList.map { it.frequency }.average()
             call.respond(mapOf("frequency" to averageValue))
         }
+
+        get("/customers/gender-dist") {
+            val response = processGet(client, url)
+            if (response == null) {
+                call.respond(HttpStatusCode.InternalServerError, "Database error")
+                return@get
+            }
+            val customerList = json.decodeFromString<ArrayList<Customer>>(response)
+            val genderDistribution = customerList
+                .mapNotNull { it.gender }
+                .groupingBy { it }
+                .eachCount()
+                .toMap()
+            call.respond(genderDistribution)
+        }
+
+        get("/customers/membership-dist"){
+            val response = processGet(client, url)
+            if (response == null) {
+                call.respond(HttpStatusCode.InternalServerError, "Database error")
+                return@get
+            }
+            val customerList = json.decodeFromString<ArrayList<Customer>>(response)
+            val membershipDistribution = customerList
+                .mapNotNull { it.membership }
+                .groupingBy { it }
+                .eachCount()
+                .toMap()
+            call.respond(membershipDistribution)
+        }
+
     }
 }
 
-suspend fun processGet(client: HttpClient, url:String):String?{
-    val response = client.get(url)
-    if (response.status != HttpStatusCode.OK)
+suspend fun processGet(client: HttpClient, url: String): String? {
+    val log = LoggerFactory.getLogger("apiLogger")
+    try {
+        val response = client.get(url)
+        if (response.status != HttpStatusCode.OK)
+            return null
+        return response.body<String>().replace(": \"\"", ": null")
+    } catch (e: Exception){
+        log.error("database is not running")
         return null
-    return response.body<String>().replace(": \"\"",": null")
+    }
+
 }
