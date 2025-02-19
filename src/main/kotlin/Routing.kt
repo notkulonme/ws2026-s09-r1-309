5dea6745ce52437633162c46671d9c02a0a2ea5e
notkulonme <nricsi006@gmail.com>
2025-02-19 13:00:58 +0100

    import optimization

--- src/main/kotlin/Routing.kt
+++ src/main/kotlin/Routing.kt
@@ -1,12 +1,7 @@
 package hu.notkulonme
 
-import io.ktor.client.*
-import io.ktor.client.engine.okhttp.*
-import io.ktor.client.request.*
-import io.ktor.serialization.kotlinx.json.*
 import io.ktor.server.application.*
 import io.ktor.server.plugins.*
-import io.ktor.server.plugins.contentnegotiation.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import java.io.File
